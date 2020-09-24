34410e53cc2609e00b1f760d0f30b7f4281acd6c
allforminers <ftresfi@gmail.com>
2020-09-24 11:05:22 +0200

    Create install_boost.sh

---
+++ install_boost.sh
@@ -0,0 +1,10 @@
+apt update
+apt install build-essential libtool automake autotools-dev autoconf pkg-config libssl-dev libgmp3-dev libevent-dev bsdmainutils wget -y
+apt install libboost-system-dev libboost-filesystem-dev libboost-chrono-dev libboost-program-options-dev libboost-test-dev libboost-thread-dev -y
+apt install software-properties-common -y
+add-apt-repository ppa:bitcoin/bitcoin -y
+apt update
+apt install libdb4.8-dev libdb4.8++-dev -y
+apt install libminiupnpc-dev -y
+apt-get install zlib1g-dev -y
+apt-get install libssl1.0-dev -y
